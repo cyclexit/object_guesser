f9778506e353d761b9f0e2ecb4414d362371704f
cyclexit <cyclexit@outlook.com>
2022-09-10 23:44:25 -0400

    Init the home page and app routes

---
+++ lib/routes.dart
@@ -0,0 +1,7 @@
+import 'package:object_guesser/home/home.dart';
+import 'package:object_guesser/about/about.dart';
+
+var appRoutes = {
+  '/': (context) => const HomeScreen(),
+  '/about': (context) => const AboutScreen()
+};
