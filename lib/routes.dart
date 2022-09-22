4af9df6a9fefbd12322ba76cbfa44b4c1e0d240d
cyclexit <cyclexit@outlook.com>
2022-09-22 13:32:49 -0400

    Implement the home screen as blank page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,11 @@
+import 'dart:js';
+
 import 'package:object_guesser/login/login.dart';
 import 'package:object_guesser/about/about.dart';
+import 'package:object_guesser/home/home.dart';
 
 var appRoutes = {
   '/': (context) => const LoginPage(),
-  '/about': (context) => const AboutPage()
+  '/about': (context) => const AboutPage(),
+  '/home': (context) => const HomePage()
 };
