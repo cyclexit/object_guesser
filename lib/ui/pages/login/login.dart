ff3db1c356bbe28c7b56e17534bd38813c687c63
cyclexit <cyclexit@outlook.com>
2022-09-29 16:03:06 -0400

    Add ui directory

---
+++ lib/ui/pages/login/login.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const _heightGap = SizedBox(
+  height: 10,
+);
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: IntrinsicWidth(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () => Navigator.pushNamedAndRemoveUntil(
+                      context, '/home', (route) => false),
+                  child: const Text("Login"),
+                ),
+                _heightGap,
+                ElevatedButton(
+                  onPressed: () => Navigator.pushNamed(context, '/about'),
+                  child: const Text("About"),
+                ),
+              ]),
+        ),
+      ),
+    );
+  }
+}
