152d9883f1b43bac52f5dee16822c133f52a06c9
Hongyi Lin <cyclexit@outlook.com>
2022-09-29 16:06:13 -0400

    Other: Add “ui” directory to hold all code about UI (#4)

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,6 @@
-import 'package:object_guesser/login/login.dart';
-import 'package:object_guesser/about/about.dart';
-import 'package:object_guesser/home/home.dart';
+import 'package:object_guesser/ui/pages/login/login.dart';
+import 'package:object_guesser/ui/pages/about/about.dart';
+import 'package:object_guesser/ui/pages/home/home.dart';
 
 var appRoutes = {
   '/': (context) => const LoginPage(),
