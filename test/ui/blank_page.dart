ed72f7e5a4945eb2da3991f3c26d400190f33c5b
cyclexit <cyclexit@outlook.com>
2022-09-30 10:49:42 -0400

    Create the BlankPage class for UI testing

---
+++ test/ui/blank_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// The blank page for UI testing.
+class BlankPage extends StatelessWidget {
+  const BlankPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
