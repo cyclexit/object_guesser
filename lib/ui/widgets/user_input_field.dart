7553e6dd0817045cd783eef2177ab076689ef25c
cyclexit <cyclexit@outlook.com>
2022-10-01 21:29:21 -0400

    Partially implement the UserInputField

---
+++ lib/ui/widgets/user_input_field.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class UserInputField extends StatelessWidget {
+  const UserInputField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      child: TextFormField(
+        autocorrect: true, // this may be a patch for user typos
+        decoration: const InputDecoration(
+          border: UnderlineInputBorder(),
+          labelText: 'Type your answer',
+        ),
+      ),
+    );
+  }
+}
