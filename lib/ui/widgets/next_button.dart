2322c360fb1b342cb98b37fd4240c29315750eb2
cyclexit <cyclexit@outlook.com>
2022-10-02 11:57:15 -0400

    Create next_button.dart

---
+++ lib/ui/widgets/next_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NextButton extends StatelessWidget {
+  void Function()? onPressed;
+
+  NextButton({super.key, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
