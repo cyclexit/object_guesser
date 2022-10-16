a5e7acb47f4782dd0a643ab67550456b6593a479
iamnotrodger <rodger.retanal24@gmail.com>
2022-10-16 18:44:18 -0400

    [feature] QuizContainer Widget

---
+++ lib/widgets/quiz_container.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:object_guesser/config/themes.dart';
+
+class QuizContainer extends StatelessWidget {
+  final Widget child;
+  final Color? color;
+
+  const QuizContainer({super.key, required this.child, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 14.0),
+      decoration: BoxDecoration(
+          color: color ?? whiteColor,
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).colorScheme.shadow,
+              spreadRadius: 0,
+              blurRadius: 12,
+              offset: const Offset(0, -15),
+            )
+          ],
+          borderRadius:
+              const BorderRadius.vertical(top: Radius.circular(30.0))),
+      child: SafeArea(
+        child: child,
+      ),
+    );
+  }
+}
