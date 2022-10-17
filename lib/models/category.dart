4ba7839feaa046de16d233f69591d140869c1c3d
iamnotrodger <rodger.retanal24@gmail.com>
2022-10-17 16:34:59 -0400

    [feature] Category Widget + Model
    * Updated the theme data to make subtitle and body text bigger

---
+++ lib/models/category.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final String id;
+  final String name;
+  final IconData icon;
+
+  const Category(
+      {required this.id, required this.name, this.icon = Icons.question_mark});
+}
