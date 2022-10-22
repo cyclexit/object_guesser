39a6073b4769aa9f1f9cd1016a9dc9d8e65b48a7
Rodger Retanal <50743708+iamnotrodger@users.noreply.github.com>
2022-10-21 21:03:31 -0400

    [update] Game Mode Header Style (#11)

--- lib/widgets/game_mode_header.dart
+++ lib/widgets/game_mode_header.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:object_guesser/models/game_mode_info.dart';
+import 'package:object_guesser/config/themes.dart';
 
 class GameModeHeader extends StatelessWidget {
   final GameModeInfo gameModeInfo;
-  const GameModeHeader({Key? key, required this.gameModeInfo})
-      : super(key: key);
+  const GameModeHeader({super.key, required this.gameModeInfo});
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     return Tooltip(
       message: gameModeInfo.gameModeDescription,
       triggerMode: TooltipTriggerMode.tap,
       child: Container(
-          padding: const EdgeInsets.all(3.0),
+          padding: const EdgeInsets.all(8.0),
           decoration: BoxDecoration(
-              color: Colors.blue,
-              border: Border.all(
-                color: Colors.blue,
-              ),
-              borderRadius: const BorderRadius.all(Radius.circular(5))),
+              color: blackColor, borderRadius: BorderRadius.circular(2.0)),
+          constraints: const BoxConstraints(minWidth: 200.0),
           child: Text(
             gameModeInfo.gameMode,
-            style: const TextStyle(color: Colors.white),
+            style: theme.textTheme.subtitle1?.apply(color: whiteColor),
+            textAlign: TextAlign.center,
           )),
     );
   }
