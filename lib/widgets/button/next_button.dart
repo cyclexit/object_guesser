1b19791e2e90b109f767ffa46bf8bf18ccc4a102
cyclexit <cyclexit@outlook.com>
2022-10-28 14:25:28 -0400

    Refactor the code of the NextButton

--- lib/widgets/button/next_button.dart
+++ lib/widgets/button/next_button.dart
@@ -1,26 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:object_guesser/widgets/button/button.dart';
 
-class NextButton extends StatelessWidget {
-  final String text;
-  final VoidCallback handlePress;
+void nextQuiz() {
+  // TODO: implement this
+}
 
-  const NextButton({super.key, this.text = "next", required this.handlePress});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-        onPressed: handlePress,
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 44.0),
-              child: Text("next"),
-            ),
-            Icon(Icons.arrow_forward)
-          ],
-        ));
-  }
+class NextButton extends Button {
+  const NextButton({super.key})
+      : super(text: "Next", icon: Icons.arrow_forward, handlePress: nextQuiz);
 }
