e95c08cf574ee3ed2e9ee276b7f87ed1a7891c7f
cyclexit <cyclexit@outlook.com>
2022-11-04 14:22:23 -0400

    Rename symbols

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -16,7 +16,7 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  static const int totalQuestions = 5;
+  static const int totalQuizes = 5;
   List<Quiz> quizList = [];
 
   final labels = const [
--- lib/services/get_quizes.dart
+++ lib/services/get_quizes.dart
@@ -1,9 +1,9 @@
 import 'package:object_guesser/models/quiz/quiz.dart';
 import 'package:object_guesser/constants/quiz_types.dart';
 
-List<Quiz> getQuizes(int totalQuiz) {
+List<Quiz> getQuizes(int totalQuizes) {
   List<Quiz> quizList = [];
-  for (int i = 0; i < totalQuiz; ++i) {
+  for (int i = 0; i < totalQuizes; ++i) {
     // quizList.add(quizTypeList[0]());
   }
   return quizList;
