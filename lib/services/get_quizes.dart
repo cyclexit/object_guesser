1bcfb4b37044595f4d6ac78013f3015379573978
cyclexit <cyclexit@outlook.com>
2022-11-04 14:20:48 -0400

    Delete some unused files

--- lib/services/get_quizes.dart
+++ lib/services/get_quizes.dart
@@ -1,25 +1,5 @@
-import 'dart:developer' as developer;
-import 'dart:math';
-
 import 'package:object_guesser/models/quiz/quiz.dart';
 import 'package:object_guesser/constants/quiz_types.dart';
-
-Quiz? getOneQuiz() {
-  // ignore: todo
-  // TODO: temporary function
-  //       remove this function when the sever is done
-  switch (Random().nextInt(quizTypeInfoMap.length)) {
-    case 0:
-      return null;
-    case 1:
-      return null;
-    case 2:
-      return null;
-    default:
-      developer.log("Unknown Quiz Type.");
-      return null;
-  }
-}
 
 List<Quiz> getQuizes(int totalQuiz) {
   List<Quiz> quizList = [];
