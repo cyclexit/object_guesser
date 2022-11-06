2943e6819bd4b0ec40ef1bcd554122e5280766cd
cyclexit <cyclexit@outlook.com>
2022-11-06 16:11:56 -0500

    Add unit test for the user input regex

---
+++ test/constants/user_input_regex_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:object_guesser/constants/user_input_regex.dart';
+
+void main() {
+  group("Test regex for trimmed user input:", () {
+    test("Valid Input", () {
+      // empty string
+      assert(trimmedUserInputRegExp.hasMatch(""));
+
+      // trimmed empty string
+      assert(trimmedUserInputRegExp.hasMatch("     ".trim()));
+
+      // single word
+      assert(trimmedUserInputRegExp.hasMatch("dog"));
+
+      // single word with hyphen
+      assert(trimmedUserInputRegExp.hasMatch("mother-in-law"));
+
+      // phrase
+      assert(trimmedUserInputRegExp
+          .hasMatch("golden retriever")); // separated by white space
+      assert(trimmedUserInputRegExp
+          .hasMatch("golden-retriever")); // separated by hyphen
+    });
+    test("Invalid Input", () {
+      // untrimmed string
+      assert(!trimmedUserInputRegExp.hasMatch("       "));
+      assert(!trimmedUserInputRegExp.hasMatch("  cat  "));
+
+      // not begin with a latin letter
+      assert(!trimmedUserInputRegExp.hasMatch("-dog"));
+      assert(!trimmedUserInputRegExp.hasMatch("_dog"));
+
+      // phrase separated by underscore
+      assert(!trimmedUserInputRegExp.hasMatch("golden_retriever"));
+
+      // phrase separated by two or more spaces
+      assert(!trimmedUserInputRegExp.hasMatch("golden  retriever"));
+      assert(!trimmedUserInputRegExp.hasMatch("golden   retriever"));
+
+      // end with a non-latin letter
+      assert(!trimmedUserInputRegExp.hasMatch("dog-"));
+      assert(!trimmedUserInputRegExp.hasMatch("       dog-"));
+      assert(!trimmedUserInputRegExp.hasMatch("dog-       "));
+      assert(!trimmedUserInputRegExp.hasMatch("dog_!@#"));
+
+      // random invalid characters
+      assert(
+          !trimmedUserInputRegExp.hasMatch("!@#\$%^&*()_+[]{}\\|;:,./<>?'\""));
+    });
+  });
+}
