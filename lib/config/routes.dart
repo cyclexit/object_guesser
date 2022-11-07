8b8499dc6d99fe608005cada3c7ac299dbdf3712
Rodger Retanal <50743708+iamnotrodger@users.noreply.github.com>
2022-11-07 15:56:01 -0500

    [feature+update] Decouple `HomePage` into separate screens (#21)
    * [feature+update] Decouple `HomePage` into separate screens
    
    - `HomePage` -> `MainPage`
    - `HomePage` home page is now a separate widget which will eventually
    contain all the things in the Figma design
    
    * [update] Added `IndexedStacked` to keep track of state

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,10 +1,10 @@
 import 'package:object_guesser/pages/login_page.dart';
 import 'package:object_guesser/pages/about_page.dart';
-import 'package:object_guesser/pages/home_page.dart';
+import 'package:object_guesser/pages/main_page.dart';
 import 'package:object_guesser/pages/quiz_page.dart';
 
 var appRoutes = {
-  HomePage.routeName: (context) => const HomePage(),
+  MainPage.routeName: (context) => const MainPage(),
   AboutPage.routeName: (context) => const AboutPage(),
   QuizPage.routeName: (context) => const QuizPage(),
   LoginPage.routeName: (context) => const LoginPage(),
