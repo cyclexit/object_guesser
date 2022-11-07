3cc9d9193dd4388bf65b1171505d281a0d0973b1
iamnotrodger <rodger.retanal24@gmail.com>
2022-11-07 15:09:03 -0500

    [feature+update] Decouple `HomePage` into separate screens
    - `HomePage` -> `MainPage`
    - `HomePage` home page is now a separate widget which will eventually
    contain all the things in the Figma design

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,60 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class HomePage extends StatefulWidget {
-  static const routeName = '/';
-
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-  static const TextStyle _optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> widgetOptions = <Widget>[
-      ElevatedButton(
+    return Center(
+      child: ElevatedButton(
           onPressed: (() => Navigator.pushNamed(context, '/quiz')),
           child: const Text("play")),
-      const Text(
-        'User Profile',
-        style: _optionStyle,
-      ),
-    ];
-
-    return Scaffold(
-      body: Center(child: widgetOptions[_selectedIndex]),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              FontAwesomeIcons.gamepad,
-              size: 20,
-            ),
-            label: 'Play',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              FontAwesomeIcons.circleUser,
-              size: 20,
-            ),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
     );
   }
 }
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:object_guesser/pages/home_page.dart';
+import 'package:object_guesser/pages/profile_page.dart';
+
+class MainPage extends StatefulWidget {
+  static const routeName = '/';
+
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+  final screens = [const HomePage(), const ProfilePage()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(30), topLeft: Radius.circular(30.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).colorScheme.shadow,
+              spreadRadius: 0,
+              blurRadius: 16,
+              offset: const Offset(0, -1),
+            )
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+          ),
+          child: BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home_rounded,
+                    size: 32,
+                  ),
+                  label: 'home'),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                  size: 32,
+                ),
+                label: 'profile',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+          ),
+        ),
+      ),
+    );
+  }
+}
