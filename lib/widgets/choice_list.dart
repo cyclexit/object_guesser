799c6ac7e7733f1210e05bc39c6220884fba0a8e
iamnotrodger <rodger.retanal24@gmail.com>
2022-11-07 11:38:01 -0500

    [update] Removed default padding from grid view

--- lib/widgets/choice_list.dart
+++ lib/widgets/choice_list.dart
@@ -33,6 +33,7 @@ class _ChoiceListState extends State<ChoiceList> {
   Widget build(BuildContext context) {
     return GridView(
       shrinkWrap: true,
+      padding: EdgeInsets.zero,
       physics: const NeverScrollableScrollPhysics(),
       gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
