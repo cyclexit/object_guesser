d5f35c342f68293b9e9b441fad7cf6c671d5dcb2
cyclexit <cyclexit@outlook.com>
2022-11-07 15:08:03 -0500

    Implement InputArea

--- lib/widgets/quiz/input_area.dart
+++ lib/widgets/quiz/input_area.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:object_guesser/constants/quiz_types.dart';
 import 'package:object_guesser/models/quizzes/input_quiz.dart';
+import 'package:object_guesser/widgets/quiz/area.dart';
+import 'package:object_guesser/widgets/quiz/quiz_image.dart';
+import 'package:object_guesser/widgets/quiz_type_text.dart';
+import 'package:object_guesser/widgets/user_input_form.dart';
 
 class InputArea extends StatelessWidget {
   final InputQuiz quiz;
@@ -12,7 +17,10 @@ class InputArea extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Area(
+      quizTypeText: const QuizTypeText(quizTypeInfo: input),
+      quizImage: QuizImage(image: quiz.image),
+      lastArea: const UserInputForm(),
+    );
   }
 }
