12ea15fbe2213f60631489d067188728fa1b19c9
cyclexit <cyclexit@outlook.com>
2022-11-23 10:56:12 -0500

    Successfully retrieve the categories

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -6,13 +6,20 @@ import 'package:object_guesser/models/label.dart';
 class FirestoreService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  Future<List<Label>> getCategories() async {
+  /// A category is a label with `root_id` and `parent_id` equal to the its own
+  /// `id`.
+  Future<List<String>> getCategories() async {
     final ref = _db.collection('labels');
     final snapshot = await ref.get();
     // log.d(snapshot);
-    final data = snapshot.docs.map((e) => e.data());
-    log.d(data);
-    List<Label> categories = [];
+    final labels = snapshot.docs.map((e) => e.data());
+    log.d(labels);
+    List<String> categories = [];
+    for (final label in labels) {
+      if (label["id"] == label["root_id"]) {
+        categories.add(label["name"]);
+      }
+    }
     return categories;
   }
 }
