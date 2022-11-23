6076ee40a7fbbfa8d0891968cc04ca413195bebc
cyclexit <cyclexit@outlook.com>
2022-11-23 13:32:38 -0500

    Upload category_icons.dart

---
+++ lib/constants/category_icons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const Map<String, IconData> categoryIconMap = {
+  "car": FontAwesomeIcons.car,
+  "dog": FontAwesomeIcons.dog,
+  "fruit": FontAwesomeIcons.raspberryPi
+};
