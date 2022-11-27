da193477f65e83eec119cf63d85bc9ea0c211532
cyclexit <cyclexit@outlook.com>
2022-11-26 21:05:47 -0500

    It seems that the user can login annoymously

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -4,6 +4,7 @@ import 'package:object_guesser/pages/home_page.dart';
 import 'package:object_guesser/pages/loading_page.dart';
 import 'package:object_guesser/pages/login_page.dart';
 import 'package:object_guesser/pages/profile_page.dart';
+import 'package:object_guesser/services/auth.dart';
 
 class MainPage extends StatefulWidget {
   static const routeName = '/';
@@ -26,60 +27,62 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(builder: ((context, snapshot) {
-      if (snapshot.connectionState == ConnectionState.waiting) {
-        return const LoadingPage();
-      } else if (snapshot.hasError) {
-        return ErrorPage(errorMessage: snapshot.error.toString());
-      } else if (snapshot.hasData) {
-        return Scaffold(
-          body: IndexedStack(index: _selectedIndex, children: screens),
-          bottomNavigationBar: Container(
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                  topRight: Radius.circular(30),
-                  topLeft: Radius.circular(30.0)),
-              boxShadow: [
-                BoxShadow(
-                  color: Theme.of(context).colorScheme.shadow,
-                  spreadRadius: 0,
-                  blurRadius: 16,
-                  offset: const Offset(0, -1),
-                )
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(30.0),
-                topRight: Radius.circular(30.0),
-              ),
-              child: BottomNavigationBar(
-                showSelectedLabels: false,
-                showUnselectedLabels: false,
-                items: const [
-                  BottomNavigationBarItem(
-                      icon: Icon(
-                        Icons.home_rounded,
-                        size: 32,
-                      ),
-                      label: 'home'),
-                  BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.person,
-                      size: 32,
-                    ),
-                    label: 'profile',
+    return StreamBuilder(
+        stream: AuthService().userStream,
+        builder: ((context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LoadingPage();
+          } else if (snapshot.hasError) {
+            return ErrorPage(errorMessage: snapshot.error.toString());
+          } else if (snapshot.hasData) {
+            return Scaffold(
+              body: IndexedStack(index: _selectedIndex, children: screens),
+              bottomNavigationBar: Container(
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      topLeft: Radius.circular(30.0)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Theme.of(context).colorScheme.shadow,
+                      spreadRadius: 0,
+                      blurRadius: 16,
+                      offset: const Offset(0, -1),
+                    )
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
                   ),
-                ],
-                currentIndex: _selectedIndex,
-                onTap: _onItemTapped,
+                  child: BottomNavigationBar(
+                    showSelectedLabels: false,
+                    showUnselectedLabels: false,
+                    items: const [
+                      BottomNavigationBarItem(
+                          icon: Icon(
+                            Icons.home_rounded,
+                            size: 32,
+                          ),
+                          label: 'home'),
+                      BottomNavigationBarItem(
+                        icon: Icon(
+                          Icons.person,
+                          size: 32,
+                        ),
+                        label: 'profile',
+                      ),
+                    ],
+                    currentIndex: _selectedIndex,
+                    onTap: _onItemTapped,
+                  ),
+                ),
               ),
-            ),
-          ),
-        );
-      } else {
-        return const LoginPage();
-      }
-    }));
+            );
+          } else {
+            return const LoginPage();
+          }
+        }));
   }
 }
