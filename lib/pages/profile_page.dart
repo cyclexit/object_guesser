6691d5b74dcd8ff9f35174daa7170167986b0a45
iamnotrodger <rodger.retanal24@gmail.com>
2022-11-28 19:17:00 -0500

    [feature] `ProfileInfoCard` Widget + `PoriflePage` layout

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:object_guesser/config/themes.dart';
+
 import 'package:object_guesser/pages/main_page.dart';
 import 'package:object_guesser/services/auth.dart';
+import 'package:object_guesser/widgets/profile_info_card.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -11,35 +12,34 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  void _signOut() async {
+    await AuthService().signOut();
+    if (mounted) {
+      Navigator.of(context)
+          .pushNamedAndRemoveUntil(MainPage.routeName, (route) => false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final user = AuthService().user;
-    return Center(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text("uid: ${user!.uid}",
-            style: Theme.of(context)
-                .textTheme
-                .headline1
-                ?.apply(color: blackColor)),
-        const SizedBox(height: 10),
-        Text("email: ${user.email ?? "None"}",
-            style: Theme.of(context)
-                .textTheme
-                .headline1
-                ?.apply(color: blackColor)),
-        ElevatedButton(
-            onPressed: () async {
-              await AuthService().signOut();
-              if (mounted) {
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                    MainPage.routeName, (route) => false);
-              }
-            },
-            child: const Text("Sign out")),
-      ],
+    return SafeArea(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ProfileInfoCard(user: user!),
+          const Spacer(),
+          ElevatedButton(
+              onPressed: _signOut,
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 15.0),
+              ),
+              child: const Text("Sign out")),
+        ],
+      ),
     ));
   }
 }
