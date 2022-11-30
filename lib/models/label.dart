89257b37850338aa9b6c691e53dde006dfbb44d9
iamnotrodger <rodger.retanal24@gmail.com>
2022-11-30 14:17:33 -0500

    [update] removed unnecessary fields for `Label` class

--- lib/models/label.dart
+++ lib/models/label.dart
@@ -1,25 +1,15 @@
 class Label {
   final String id;
-  final String parentId;
-  final String rootId;
   final String name;
 
-  const Label(
-      {required this.id,
-      required this.parentId,
-      required this.rootId,
-      required this.name});
+  const Label({required this.id, this.name = ""});
 
   Label.fromJson(Map<String, dynamic> json)
       : id = json["id"],
-        parentId = json["parent_id"],
-        rootId = json["root_id"],
-        name = json["name"];
+        name = json["name"] ?? "";
 
   Map<String, dynamic> toJson() => {
         "id": id,
-        "parent_id": parentId,
-        "root_id": rootId,
         "name": name,
       };
 }
