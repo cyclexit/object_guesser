770610dab22a837e7ea84b7c2c4a741ec7183190
cyclexit <cyclexit@outlook.com>
2022-12-01 22:31:06 -0500

    Add collection names

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,6 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'package:object_guesser/models/category.dart';
 import 'package:object_guesser/models/label.dart';
+import 'package:object_guesser/models/quizzes/quiz.dart';
+
+class _Collections {
+  static const String images = "images";
+  static const String labels = "labels";
+  static const String imageLabelRecords = "image_label_records";
+  static const String multipleChoiceQuizzes = "multiple_choice_quizzes";
+  static const String inputQuizzes = "input_quizzes";
+  static const String selectionQuizzes = "selection_quizzes";
+  static const String games = "games";
+}
 
 class FirestoreService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -18,5 +30,10 @@ class FirestoreService {
       }
     }
     return categories;
+  }
+
+  Future<List<Quiz>> getQuizzes(int totalQuizzes, Category category) async {
+    // TODO: implement this
+    return [];
   }
 }
