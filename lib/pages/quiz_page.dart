d76f2dbacee9a1f13044b0c4753e7ee2259118cb
cyclexit <cyclexit@outlook.com>
2022-12-04 17:08:27 -0500

    Resolve TODO

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -22,8 +22,6 @@ class QuizPage extends StatefulWidget {
   static const routeName = '/quiz';
   final Category category;
 
-  /// TODO: add `gameId` here
-
   const QuizPage({super.key, required this.category});
 
   @override
