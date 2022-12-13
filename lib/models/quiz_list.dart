b4e20bd6635b557080e1484f7f3ee084c55fb1b6
cyclexit <cyclexit@outlook.com>
2022-12-12 19:24:05 -0500

    Update quiz list model

--- lib/models/quiz_list.dart
+++ lib/models/quiz_list.dart
@@ -4,23 +4,26 @@ import 'package:object_guesser/models/quizzes/quiz.dart';
 
 class QuizList extends ChangeNotifier {
   static const int totalQuizzes = 10;
-  late int _idx;
+  int _idx = 0;
+  int _currentPoints = 0;
   final List<Quiz> _quizzes = [];
 
   bool get isNotReady => _quizzes.isEmpty;
   bool get isReady => _quizzes.isNotEmpty;
   bool get isRunning => _idx < _quizzes.length;
   bool get isDone => _idx >= _quizzes.length;
+  List<Quiz> get quizzes => _quizzes;
   Quiz get currentQuiz => _quizzes[_idx];
+  int get currentIndex => _idx;
+  int get currentPoints => _currentPoints;
 
   set quizzes(List<Quiz> quizzes) {
-    _idx = 0;
     _quizzes.addAll(quizzes);
     notifyListeners();
   }
 
   void next() {
-    ++_idx;
+    _currentPoints += _quizzes[_idx++].getPoints();
     notifyListeners();
   }
 }
