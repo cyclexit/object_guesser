e129098a3563cb7ea612799b6d4b1558922d71fb
iamnotrodger <rodger.retanal24@gmail.com>
2022-12-13 09:05:20 -0500

    [update] `QuizProvider` on `InputQuiz`

--- lib/widgets/quiz_body/input_body.dart
+++ lib/widgets/quiz_body/input_body.dart
@@ -2,18 +2,22 @@ import 'package:flutter/material.dart';
 
 import 'package:object_guesser/constants/quiz_types.dart';
 import 'package:object_guesser/models/quizzes/input_quiz.dart';
+import 'package:object_guesser/provider/quiz_provider.dart';
 import 'package:object_guesser/widgets/quiz_body/quiz_body.dart';
 import 'package:object_guesser/widgets/quiz_image.dart';
 import 'package:object_guesser/widgets/quiz_type_text.dart';
 import 'package:object_guesser/widgets/user_input_form.dart';
+import 'package:provider/provider.dart';
 
 class InputBody extends StatelessWidget {
   final InputQuiz quiz;
 
   const InputBody({super.key, required this.quiz});
 
-  void setAnswer(String? userInput) {
-    quiz.answer = userInput;
+  Function(String?) setAnswer(BuildContext context) {
+    return (String? userInput) =>
+        Provider.of<QuizProvider>(context, listen: false)
+            .setAnswer(quiz, userInput);
   }
 
   @override
@@ -21,7 +25,8 @@ class InputBody extends StatelessWidget {
     return QuizBody(
       type: const QuizTypeText(quizTypeInfo: input),
       image: QuizImage(image: quiz.image),
-      bottomArea: UserInputForm(onSaved: setAnswer),
+      bottomArea:
+          UserInputForm(onSaved: setAnswer(context), answer: quiz.answer),
     );
   }
 }
