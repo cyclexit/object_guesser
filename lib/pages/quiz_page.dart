d020c70fcacf6b28c38270d1d74a0124760861a4
cyclexit <cyclexit@outlook.com>
2022-12-12 19:51:48 -0500

    Almost there

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -1,13 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-import 'package:object_guesser/log.dart';
 import 'package:object_guesser/config/themes.dart';
+import 'package:object_guesser/log.dart';
 import 'package:object_guesser/models/category.dart';
+import 'package:object_guesser/models/quiz_list.dart';
 import 'package:object_guesser/models/quizzes/input_quiz.dart';
 import 'package:object_guesser/models/quizzes/multiple_choice_quiz.dart';
 import 'package:object_guesser/models/quizzes/quiz.dart';
 import 'package:object_guesser/models/quizzes/selection_quiz.dart';
+import 'package:object_guesser/pages/error_page.dart';
 import 'package:object_guesser/pages/loading_page.dart';
 import 'package:object_guesser/pages/main_page.dart';
 import 'package:object_guesser/services/firestore_service.dart';
@@ -18,130 +20,53 @@ import 'package:object_guesser/widgets/quiz_body/multiple_choice_body.dart';
 import 'package:object_guesser/widgets/quiz_body/selection_body.dart';
 import 'package:object_guesser/widgets/quiz_container.dart';
 import 'package:object_guesser/widgets/quiz_header.dart';
+import 'package:provider/provider.dart';
 
-class QuizPage extends StatefulWidget {
-  static const routeName = '/quiz';
+class _QuizPage extends StatelessWidget {
+  final String gameId;
   final Category category;
 
-  const QuizPage({super.key, required this.category});
-
-  @override
-  State<QuizPage> createState() => _QuizPageState();
-}
-
-class _QuizPageState extends State<QuizPage> {
-  // NOTE: keep _totalQuizzes small for dev
-  static const int _totalQuizzes = 10;
-
-  String _gameId = "";
-  List<Quiz> _quizzes = [];
-  int _idx = 0;
-  int _points = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    FirestoreService().getQuizzes(_totalQuizzes, widget.category).then((value) {
-      _gameId = value["game_id"];
-      setState(() {
-        _quizzes = value["quizzes"];
-      });
-    }, onError: (error) {
-      log.e(error);
-    });
-  }
-
-  void _handleNextQuiz() {
-    setState(() {
-      _points += _quizzes[_idx].getPoints();
-      _idx++;
-    });
-  }
-
-  Widget _updateQuizBody() {
-    Type quizType = _quizzes[_idx].runtimeType;
-    Quiz quiz = _quizzes[_idx];
-
-    switch (quizType) {
-      case MultipleChoiceQuiz:
-        return MultipleChoiceBody(quiz: quiz as MultipleChoiceQuiz);
-      case InputQuiz:
-        return InputBody(quiz: quiz as InputQuiz);
-      case SelectionQuiz:
-        return SelectionBody(quiz: quiz as SelectionQuiz);
-    }
-    log.e("Unkown quiz type!");
-    return Container();
-  }
-
-  /// Validate the user performance in this game.
-  ///
-  /// Calculate the points acquired by the user for `MultipleChoiceQuiz` and
-  /// `SelectionQuiz` as `userValidationPoints`.
-  ///
-  /// If `userValidationPoints` / `totalMaxPoints` >= 0.5, then the user game
-  /// performance is considered as valid.
-  bool _validateUserPerformance(List<Quiz> quizzes) {
-    const double validationRatio = 0.5;
-    int totalMaxPoints = 0;
-    int userValidationPoints = 0;
-    for (final quiz in quizzes) {
-      if (quiz.runtimeType == MultipleChoiceQuiz) {
-        final q = quiz as MultipleChoiceQuiz;
-        totalMaxPoints += q.maxPoints;
-        userValidationPoints += q.getPoints();
-      } else if (quiz.runtimeType == SelectionQuiz) {
-        final q = quiz as SelectionQuiz;
-        totalMaxPoints += q.maxPoints;
-        userValidationPoints += q.getPoints();
-      }
-    }
-    return (userValidationPoints / totalMaxPoints) >= validationRatio;
-  }
+  const _QuizPage({required this.gameId, required this.category});
 
   void _exitQuiz(BuildContext context) {
-    Timestamp finishTime = Timestamp.now();
-    for (final quiz in _quizzes) {
-      FirestoreService().uploadUserQuizRecord(
-          quiz.id, quiz.runtimeType, quiz.getPoints(), quiz.answer, finishTime);
-    }
-    FirestoreService().updateUserGameHistory(
-        _gameId, _points, widget.category.name, finishTime);
-    if (_validateUserPerformance(_quizzes)) {
-      log.d("The user is validated for this game"); // debug
-      FirestoreService().updateImageLabelRecords(_quizzes);
+    final quizList = Provider.of<QuizList>(context);
+    if (quizList.isDone) {
+      Timestamp finishTime = Timestamp.now();
+      for (final quiz in quizList.quizzes) {
+        FirestoreService().uploadUserQuizRecord(quiz.id, quiz.runtimeType,
+            quiz.getPoints(), quiz.answer, finishTime);
+      }
+      FirestoreService().updateUserGameHistory(
+          gameId, quizList.currentPoints, category.name, finishTime);
+      if (quizList.validateUserPerformance()) {
+        log.d("The user is validated for this game"); // debug
+        FirestoreService().updateImageLabelRecords(quizList.quizzes);
+      }
     }
     Navigator.popUntil(context, ModalRoute.withName(MainPage.routeName));
   }
 
-  int _calculateScore() {
-    int score = 0;
-    for (var quiz in _quizzes) {
-      score += quiz.getPoints();
-    }
-    return score;
-  }
-
   @override
   Widget build(BuildContext context) {
+    final quizList = Provider.of<QuizList>(context);
     Widget body;
-
-    if (_quizzes.isEmpty) {
-      body = const LoadingPage();
-    } else if (_idx < _quizzes.length) {
+    if (quizList.isNotReady) {
+      return const LoadingPage();
+    } else if (quizList.isRunning) {
       body = Column(
         children: [
           SafeArea(
               bottom: false,
               child: Column(
                 children: [
+                  // TODO: change API and use consumer
                   QuizHeader(
-                    category: widget.category,
+                    category: category,
                     exitQuiz: () => _exitQuiz(context),
-                    points: _points,
+                    points: quizList.currentPoints,
                   ),
                   const SizedBox(height: 12.0),
-                  ProgressBar(quizzes: _quizzes, index: _idx)
+                  // ProgressBar(quizzes: _quizzes, index: _idx)
                 ],
               )),
           const SizedBox(height: 16.0),
@@ -151,16 +76,31 @@ class _QuizPageState extends State<QuizPage> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                  _updateQuizBody(),
+                  Consumer<QuizList>(
+                    builder: (context, qList, child) {
+                      Quiz quiz = qList.currentQuiz;
+                      switch (quiz.runtimeType) {
+                        case MultipleChoiceQuiz:
+                          return MultipleChoiceBody(
+                              quiz: quiz as MultipleChoiceQuiz);
+                        case InputQuiz:
+                          return InputBody(quiz: quiz as InputQuiz);
+                        case SelectionQuiz:
+                          return SelectionBody(quiz: quiz as SelectionQuiz);
+                      }
+                      log.e("Unkown quiz type!");
+                      return Container();
+                    },
+                  ),
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 50.0),
-                    child: NextButton(handlePress: _handleNextQuiz),
+                    child: NextButton(handlePress: quizList.next),
                   ),
                 ])),
           ),
         ],
       );
-    } else {
+    } else if (quizList.isDone) {
       body = SizedBox(
           width: double.infinity,
           child: Column(
@@ -172,7 +112,7 @@ class _QuizPageState extends State<QuizPage> {
                         .textTheme
                         .headline2
                         ?.apply(color: whiteColor)),
-                Text(_calculateScore().toString(),
+                Text(quizList.currentPoints.toString(),
                     style: Theme.of(context)
                         .textTheme
                         .subtitle1
@@ -181,9 +121,39 @@ class _QuizPageState extends State<QuizPage> {
                     onPressed: () => _exitQuiz(context),
                     child: const Text("go back home"))
               ]));
+    } else {
+      body = const Center(child: Text("Oops! Something is wrong...🤡"));
     }
-
     return Scaffold(
         backgroundColor: Theme.of(context).colorScheme.primary, body: body);
+  }
+}
+
+class QuizPage extends StatelessWidget {
+  static const routeName = '/quiz';
+  final Category category;
+  final QuizList quizList = QuizList();
+
+  QuizPage({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: FirestoreService().getQuizzes(QuizList.totalQuizzes, category),
+        builder: ((context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LoadingPage();
+          } else if (snapshot.hasError) {
+            return ErrorPage(errorMessage: snapshot.error.toString());
+          } else if (snapshot.hasData) {
+            final gameData = snapshot.data!;
+            quizList.quizzes = gameData["quizzes"];
+            return ChangeNotifierProvider(
+              create: ((context) => quizList),
+              child: _QuizPage(gameId: gameData["game_id"], category: category),
+            );
+          }
+          return ErrorPage(errorMessage: snapshot.toString());
+        }));
   }
 }
